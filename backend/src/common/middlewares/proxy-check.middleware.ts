7c66f32ca947a4d2f97d2558558967ef5f12f2c8
Ali Movahedi <movahedi.4li@gmail.com>
2026-02-12 15:41:03 +0330

    fix: disable https required

--- backend/src/common/middlewares/proxy-check.middleware.ts
+++ backend/src/common/middlewares/proxy-check.middleware.ts
@@ -7,22 +7,23 @@ import { isDevelopment } from '@common/utils/startup-app';
 const logger = new Logger('ProxyCheckMiddleware');
 
 export function proxyCheckMiddleware(req: Request, res: Response, next: NextFunction) {
-    if (isDevelopment()) {
-        return next();
-    }
-
-    const isProxy = Boolean(req.headers['x-forwarded-for']);
-    const isHttps = Boolean(req.headers['x-forwarded-proto'] === 'https');
-
-    logger.debug(
-        `X-Forwarded-For: ${req.headers['x-forwarded-for']}, X-Forwarded-Proto: ${req.headers['x-forwarded-proto']}`,
-    );
-
-    if (!isHttps || !isProxy) {
-        res.socket?.destroy();
-        logger.error('Reverse proxy and HTTPS are required.');
-        return;
-    }
-
+  return next();
+  if (isDevelopment()) {
     return next();
+  }
+
+  const isProxy = Boolean(req.headers['x-forwarded-for']);
+  const isHttps = Boolean(req.headers['x-forwarded-proto'] === 'https');
+
+  logger.debug(
+    `X-Forwarded-For: ${req.headers['x-forwarded-for']}, X-Forwarded-Proto: ${req.headers['x-forwarded-proto']}`,
+  );
+
+  if (!isHttps || !isProxy) {
+    res.socket?.destroy();
+    logger.error('Reverse proxy and HTTPS are required.');
+    return;
+  }
+
+  return next();
 }
